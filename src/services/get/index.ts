8aaf7ec4bdc9387629c9acc9a3dd8d63b8259674
Emmanuel Festus <120015643+Aikay-dev@users.noreply.github.com>
2024-07-28 13:07:32 +0100

    updated fetch calls to no cache

--- src/services/get/index.ts
+++ src/services/get/index.ts
@@ -1,18 +1,21 @@
 import { Iparts, Itruck } from "@/lib/types";
 import { BASE_URL } from "@/server/lib/api";
 import { Driver, Part, Truck } from "@prisma/client";
-import axios from "axios";
+// import axios from "axios";
 
 export async function getTrucksSell() {
   try {
-    const res = await axios.get(`${BASE_URL}/api/trucks`, {
+    const res = await fetch(`${BASE_URL}/api/trucks`, {
+      cache: "no-store",
       headers: {
         "Content-Type": "application/json",
         "x-purpose": "Sell" as Itruck["purpose"],
       },
     });
 
-    return res.data as Truck[];
+    const data = await res.json();
+
+    return data as Truck[];
   } catch (error) {
     console.error(error);
     return [];
@@ -21,14 +24,16 @@ export async function getTrucksSell() {
 
 export async function getTrucksHire() {
   try {
-    const res = await axios.get(`${BASE_URL}/api/trucks`, {
+    const res = await fetch(`${BASE_URL}/api/trucks`, {
+      cache: "no-store",
       headers: {
         "Content-Type": "application/json",
         "x-purpose": "Hire" as Itruck["purpose"],
       },
     });
+    const data = await res.json();
 
-    return res.data as Truck[];
+    return data as Truck[];
   } catch (error) {
     console.error(error);
     return [];
@@ -37,9 +42,12 @@ export async function getTrucksHire() {
 
 export async function getParts() {
   try {
-    const res = await axios.get(`${BASE_URL}/api/parts`);
+    const res = await fetch(`${BASE_URL}/api/parts`, {
+      cache: "no-store",
+    });
+    const data = await res.json();
 
-    return res.data as Part[];
+    return data as Part[];
   } catch (error) {
     console.error(error);
     return [];
@@ -48,9 +56,12 @@ export async function getParts() {
 
 export async function getDrivers() {
   try {
-    const res = await axios.get(`${BASE_URL}/api/driver`);
+    const res = await fetch(`${BASE_URL}/api/driver`, {
+      cache: "no-store",
+    });
+    const data = await res.json();
 
-    return res.data as Driver[];
+    return data as Driver[];
   } catch (error) {
     console.error(error);
     return [];
